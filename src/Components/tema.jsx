22604e70dbcd3bf2d422c57a96e43f2279b9ef29
KristijanV <kvuje60@gmail.com>
2023-11-19 18:45:50 +0100

    ..

--- src/Components/tema.jsx
+++ src/Components/tema.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Theme } from "../App";
 
 const Tema = () => {
   const [theme, setTheme] = useContext(Theme);
-  const [counter, setCounter] = useState(0);
+
   return (
     <div className={`${theme}`}>
       <label className="switch">
@@ -12,7 +12,6 @@ const Tema = () => {
           checked={theme === "dark"}
           onChange={(e) => {
             setTheme(e.target.checked ? "dark" : "light");
-            setCounter(counter + 1);
           }}
         />
         <span className="slider round"></span>
@@ -24,8 +23,6 @@ const Tema = () => {
           )}
         </div>
       </label>
-      <p>You are using {theme} mode!</p>
-      <p>You've switched mode {counter} times.</p>
     </div>
   );
 };
